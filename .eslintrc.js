be741f4db0f61089106a210d8712fb5be08034ac
Lannis <nphoangtu@gmail.com>
2024-11-09 20:06:16 +0700

    update: add env example

--- .eslintrc.js
+++ .eslintrc.js
@@ -9,7 +9,6 @@ module.exports = {
     'plugin:@typescript-eslint/eslint-recommended',
     'plugin:@typescript-eslint/recommended',
     'prettier',
-    'prettier/@typescript-eslint',
   ],
   root: true,
   env: {
@@ -21,5 +20,10 @@ module.exports = {
     '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/explicit-module-boundary-types': 'off',
     '@typescript-eslint/no-explicit-any': 'off',
+    '@typescript-eslint/no-unused-vars': [
+      'error',
+      { argsIgnorePattern: '^_', varsIgnorePattern: '^_' },
+    ],
+    'no-console': 'error',
   },
 };
--- src/modules/database/database.module.ts
+++ src/modules/database/database.module.ts
@@ -7,32 +7,32 @@ import { ConfigServiceKeys } from '../../common/constants';
 export const logYellow = (message: string) => `\x1b[33m${message}\x1b[0m`;
 export const logRed = (message: string) => `\x1b[91m${message}\x1b[0m`;
 
-const changeColors = (query: string, texts: string[], color = logYellow) => {
-  let result = query;
-  texts.forEach((text) => {
-    result = result.replace(new RegExp(text, 'g'), color(text));
-  });
-  return result;
-};
-const logging = (sql: string) => {
-  let query = sql.replace('WHERE', '\n\tWHERE');
-  query = query.replace('FROM', '\n\tFROM');
-  query = query.replace('JOIN', '\n\tJOIN');
-  query = query.replace('ORDER', '\n\tORDER');
+// const changeColors = (query: string, texts: string[], color = logYellow) => {
+//   let result = query;
+//   texts.forEach((text) => {
+//     result = result.replace(new RegExp(text, 'g'), color(text));
+//   });
+//   return result;
+// };
+// const logging = (sql: string) => {
+//   let query = sql.replace('WHERE', '\n\tWHERE');
+//   query = query.replace('FROM', '\n\tFROM');
+//   query = query.replace('JOIN', '\n\tJOIN');
+//   query = query.replace('ORDER', '\n\tORDER');
 
-  query = changeColors(query, [
-    'SELECT',
-    'OR ',
-    'WHERE',
-    'FROM',
-    'JOIN',
-    'UPDATE',
-  ]);
-  query = changeColors(query, ['ORDER BY', 'ON', 'AND']);
-  query = changeColors(query, ['LEFT', 'RIGHT', 'INNER']);
-  query = changeColors(query, ['Executed'], logRed);
-  console.log(logRed('*'.repeat(7)), query);
-};
+//   query = changeColors(query, [
+//     'SELECT',
+//     'OR ',
+//     'WHERE',
+//     'FROM',
+//     'JOIN',
+//     'UPDATE',
+//   ]);
+//   query = changeColors(query, ['ORDER BY', 'ON', 'AND']);
+//   query = changeColors(query, ['LEFT', 'RIGHT', 'INNER']);
+//   query = changeColors(query, ['Executed'], logRed);
+//   console.log(logRed('*'.repeat(7)), query);
+// };
 
 const sequelizeModule = SequelizeModule.forRootAsync({
   inject: [ConfigService],
